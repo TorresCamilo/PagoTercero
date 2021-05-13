717bd4ad6bf3805e6c21610a3ace93d879c7501f
Camilo Torres <camiloatm@hotmail.com>
2021-05-12 19:41:20 -0500

    Realizado los principales componentes, routing, clases. Dentro de frontend

---
+++ PagoTercero/ClientApp/src/app/Tercero/models/tercero.ts
@@ -0,0 +1,5 @@
+export class Tercero {
+  identificacion: string;
+  nombre: string;
+  telefono: string;
+}
--- PagoTercero/ClientApp/src/app/app.module.ts
+++ PagoTercero/ClientApp/src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule} from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { TerceroRegistroComponent } from './Tercero/tercero-registro/tercero-registro.component';
-import { TerceroConsultaComponent } from './Tercero/tercero-consulta/tercero-consulta.component';
-import { PagoConsultaComponent } from './Pago/pago-consulta/pago-consulta.component';
+import { AppComponent } from "./app.component";
+import { NavMenuComponent } from "./nav-menu/nav-menu.component";
+import { HomeComponent } from "./home/home.component";
+import { CounterComponent } from "./counter/counter.component";
+import { FetchDataComponent } from "./fetch-data/fetch-data.component";
+import { TerceroRegistroComponent } from "./Tercero/tercero-registro/tercero-registro.component";
+import { TerceroConsultaComponent } from "./Tercero/tercero-consulta/tercero-consulta.component";
+import { PagoConsultaComponent } from "./Pago/pago-consulta/pago-consulta.component";
+import { PagoRegistroComponent } from "./Pago/pago-registro/pago-registro.component";
 
 @NgModule({
   declarations: [
@@ -22,19 +23,24 @@ import { PagoConsultaComponent } from './Pago/pago-consulta/pago-consulta.compon
     FetchDataComponent,
     TerceroRegistroComponent,
     TerceroConsultaComponent,
-    PagoConsultaComponent
+    PagoConsultaComponent,
+    PagoRegistroComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
-    { path: '', component: HomeComponent, pathMatch: 'full' },
-    { path: 'counter', component: CounterComponent },
-    { path: 'fetch-data', component: FetchDataComponent },
-], { relativeLinkResolution: 'legacy' })
+    RouterModule.forRoot(
+      [
+        { path: "", component: TerceroRegistroComponent, pathMatch: "full" },
+        { path: "tercero/consulta", component: TerceroConsultaComponent },
+        { path: "pago/registro", component: PagoRegistroComponent },
+        { path: "pago/consulta", component: PagoConsultaComponent },
+      ],
+      { relativeLinkResolution: "legacy" }
+    ),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
